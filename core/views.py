028f56e8b5eb4eb91fc896b5c1355dc1b9213e5c
Overlord-zerg-more-minerals <fool_fook_hook12345@mail.ru>
2020-07-25 04:28:21 +0600

    add create product(test)

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, HttpResponse
-
-def home(request):
-    return HttpResponse('homepage')
+from django.shortcuts import render,\
+    HttpResponse, redirect
+from product.views import products
 
 
 def test(request):
     return HttpResponse('test')
+
+
+def home(request):
+    return redirect('products')
--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('all', products, name="products"),
-    path('<int:id>/', product, name='product')
+    path('<int:id>/', product, name='product'),
+    path('create/', create_product, name='create-product')
 ]
