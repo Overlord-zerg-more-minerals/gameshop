be8d3ed02a276042e7298b876e9e8830ddfdf7b8
Overlord-zerg-more-minerals <fool_fook_hook12345@mail.ru>
2020-08-01 18:18:15 +0600

    add registration

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path('', home, name="home"),
     path('test/', test, name="test"),
-    path('login/', login, name="login")
+    path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
+    path('registration/', registration, name="registration")
 ]
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,\
     HttpResponse, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
-
+from core.forms import RegistrationForm
 
 def test(request):
     return HttpResponse('test')
@@ -13,6 +13,8 @@ def home(request):
 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect(home)
     context = {}
     if "login" in request.POST:
         form = AuthenticationForm(request, request.POST)
@@ -23,3 +25,20 @@ def login(request):
 
     context["form"] = AuthenticationForm()
     return render(request, "core/login.html", context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect(home)
+
+
+def registration(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(home)
+            
+    context = {}
+    context["form"] = RegistrationForm()
+    return render(request, "core/registration.html", context)
