ef48eb1f69ebcbf255aca39326f4c333bd236a1f
Overlord-zerg-more-minerals <fool_fook_hook12345@mail.ru>
2020-08-06 16:59:54 +0600

    modified product admin

--- product/admin.py
+++ product/admin.py
@@ -2,15 +2,21 @@ from django.contrib import admin
 from product.models import *
 
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    model = Category
-    # fields = ["title", "user", "category", "price"]
-    list_display = ["id", "title", "user", "category", "price"]
+    model = Product
+    readonly_fields = [
+        "price",
+        "user",
+        "quantity_purchases",
+        "availability"
+    ]
+    # цикл для отображения всех полей [field.name for field in Product._meta.get_fields()]
+    list_display = ["title", "id", "user", "category", "price"]
     search_fields = ["title", "description", "user__username"]
     list_filter = ["category"]
-    
+    list_editable = ["price"]
 
-admin.site.register(Product, ProductAdmin)
 
 
 admin.site.register(Category)
